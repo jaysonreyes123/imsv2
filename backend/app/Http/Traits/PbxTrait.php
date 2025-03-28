a51a53fbaa4a4bbf7f384f121e1331079d4aca46
jaysonreyes123 <jaysonreyes012345@gmail.com>
2025-03-28 10:59:53 +0800

    upload

--- backend/app/Http/Traits/PbxTrait.php
+++ backend/app/Http/Traits/PbxTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Traits;
 
+use App\Models\CallLog;
 use App\Models\PbxToken;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
@@ -11,14 +12,32 @@ trait PbxTrait
     //
     public function call_logs(){
         $access_token = $this->check_token_expiration();
-        $start_time = Carbon::now()->format("m/d/Y 12:00:00 AM");
-        $end_time = Carbon::now()->format("m/d/Y 11:59:59 PM");
+        $start_time = Carbon::now()->format("m/d/Y")."  12:00:00 AM";
         $request = $this->http_request()->get(env('YEASTAR_API')."/cdr/search",[
             "access_token" => $access_token,
             "start_time"   => $start_time,
-            "end_time"     => $end_time
         ]);
-        return $request;
+        if($request['errcode'] == 0){
+            foreach($request['data'] as $call_log){
+                $calllogs_tks_dateandtime   = $call_log->time;
+                $calllogs_tks_fromno        = $call_log->call_from_number;
+                $calllogs_tks_tono          = $call_log->call_to_number;
+                $calllogs_tks_duration      = $call_log->duration;
+                $call_logs_model = CallLog::where('date_and_time',$calllogs_tks_dateandtime)
+                ->where('from_no',$calllogs_tks_fromno)
+                ->where('to_no',$calllogs_tks_tono)
+                ->where('duration',$calllogs_tks_duration)->get();
+                if($call_logs_model->count() == 0){
+                    $insert_model = new CallLog();
+                    $insert_model->date_and_time = $calllogs_tks_dateandtime;
+                    $insert_model->from_no = $calllogs_tks_fromno;
+                    $insert_model->to_no = $calllogs_tks_tono;
+                    $insert_model->duration = $calllogs_tks_duration;
+                    $insert_model->source = '3CX';
+                    $insert_model->save();
+                }
+            }
+        }
     }
     public function check_token_expiration(){
         $pbx_model = PbxToken::where('name','yeastar')->first();
